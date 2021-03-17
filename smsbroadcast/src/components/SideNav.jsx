fbe6fa14cbc7227c14cedf183a1ee9f4a027c4da
adhim1st <adhim@student.uns.ac.id>
2021-03-17 15:04:19 +0700

    sidenav styling done

--- smsbroadcast/src/components/SideNav.jsx
+++ smsbroadcast/src/components/SideNav.jsx
@@ -1,20 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import navLogo from "../img/OCA-logo.svg";
 import homeIcon from "../img/house-black-silhouette-without-door.svg";
 import messageIcon from "../img/chat-bubbles-with-ellipsis.svg";
-const sideNav = () => {
+import dropdownIcon from "../img/Rectangle 557.svg";
+import arrowUp from "../img/arrowup.svg";
+import arrowDown from "../img/arrowdown.svg";
+const SideNav = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleShowMenu = () => {
+    setShowMenu((prevCheck) => !prevCheck);
+  };
   return (
     <nav className="sideNav-container">
-      <img src={navLogo} alt="" />
+      <img src={navLogo} alt="navLogo" />
       <div className="sideNav-menu">
         <NavLink className="btn" to="/home" activeClassName="btn-active">
-          <img src={homeIcon} alt="" />
-          <p>Home</p>
+          <img src={homeIcon} alt="homeIcon" />
+          <span>Home</span>
         </NavLink>
+        <div className="dropdown-menu">
+          <a className="dropdown-btn" onClick={handleShowMenu}>
+            <img src={messageIcon} alt="messageIcon" />
+            <span>SMS</span>
+            {showMenu ? (
+              <img src={arrowUp} className="dropdown-icon" />
+            ) : (
+              <img src={arrowDown} className="dropdown-icon" />
+            )}
+          </a>
+          {showMenu ? (
+            <div className="sms-dropdown">
+              <NavLink
+                to="/broadcast"
+                className="dropdown-sub"
+                activeClassName="btn-sub-active"
+              >
+                <img src={dropdownIcon} alt="dropdownIcon" />
+                <span>Broadcast</span>
+              </NavLink>
+              <NavLink
+                to="sms-scheduller"
+                className="dropdown-sub"
+                activeClassName="btn-sub-active"
+              >
+                <img src={dropdownIcon} alt="dropdownIcon" />
+                <span>SMS Scheduller</span>
+              </NavLink>
+            </div>
+          ) : null}
+        </div>
       </div>
     </nav>
   );
 };
 
-export default sideNav;
+export default SideNav;
