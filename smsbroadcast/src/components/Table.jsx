7a4a776a04f1744faab220e7ef9ab7b6bc63e778
adhim1st <adhim@student.uns.ac.id>
2021-03-17 11:50:35 +0700

    Add Recent BLAST Table : - create table scroll - dynamic status color

--- smsbroadcast/src/components/Table.jsx
+++ smsbroadcast/src/components/Table.jsx
@@ -1,7 +1,67 @@
 import React from "react";
-
+import sortIcon from "../img/sort.svg";
+import filterIcon from "../img/filter.svg";
+import tableData from "../data/tableData";
 const Table = () => {
-  return <div className="table-container"></div>;
+  return (
+    <div className="table-container">
+      <div className="table-header">
+        <h2>Recent BLAST</h2>
+        <div className="table-function">
+          <div className="sort">
+            <img src={sortIcon} alt="sort" />
+            <p>Sort</p>
+          </div>
+          <div className="filter">
+            <img src={filterIcon} alt="filter" />
+            <p>Filter</p>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="table-content">
+        <table className="blast-table">
+          <thead>
+            <tr>
+              <th>id name</th>
+              <th>type</th>
+              <th>campaign</th>
+              <th>total blast</th>
+              <th>status</th>
+              <th>date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.map((data) => {
+              return (
+                <tr>
+                  <td>{data.id_name}</td>
+                  <td>{data.type}</td>
+                  <td>{data.campaign}</td>
+                  <td>{data.total_blast}</td>
+                  <td
+                    id="status"
+                    className={
+                      data.status === "Success"
+                        ? "status-green"
+                        : data.status === "Failed"
+                        ? "status-red"
+                        : data.status === "Pending"
+                        ? "status-yellow"
+                        : "status-purple"
+                    }
+                  >
+                    {data.status}
+                  </td>
+                  <td>{data.date}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Table;
---
+++ smsbroadcast/src/data/tableData.js
@@ -0,0 +1,52 @@
+const tableData = [
+  {
+    id_name: "MTN_EGY-LIFE",
+    type: "Call",
+    campaign: "Reminding Billing 20 September",
+    total_blast: "1255",
+    status: "Success",
+    date: "July 15, 2018 4:47 AM",
+  },
+  {
+    id_name: "MTN_BCA",
+    type: "Call",
+    campaign: "Reminding Billing 20 November",
+    total_blast: "600",
+    status: "Failed",
+    date: "August 3, 2012 5:26 PM",
+  },
+  {
+    id_name: "RTL1_BPNA",
+    type: "SMS",
+    campaign: "Pengingat Pagi",
+    total_blast: "121223",
+    status: "Pending",
+    date: "October 12, 2018 1:47 AM",
+  },
+  {
+    id_name: "KUI_AICE",
+    type: "Email",
+    campaign: "Test",
+    total_blast: "958433",
+    status: "Progress",
+    date: "March 13, 2016 5:25 PM",
+  },
+  {
+    id_name: "RTL1_BANKBJB",
+    type: "Email",
+    campaign: "Coba",
+    total_blast: "3313",
+    status: "Success",
+    date: "July 14, 2019 8:39 PM",
+  },
+  {
+    id_name: "MTN_EGY-LIFE",
+    type: "Call",
+    campaign: "Reminding Billing 20 September",
+    total_blast: "1255",
+    status: "Success",
+    date: "July 15, 2018 4:47 AM",
+  },
+];
+
+export default tableData;
